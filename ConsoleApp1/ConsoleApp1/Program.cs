94c3f51d2cd3af472d83bb92bade6961fa56cfe2
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-09-26 16:13:08 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace vazao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int volumeInicial, vazao, tempo, volumeFinal;
+            volumeInicial = int.Parse(Console.ReadLine());
+            vazao = int.Parse(Console.ReadLine());
+            tempo = int.Parse(Console.ReadLine());
+            if (vazao * tempo > volumeInicial)
+            {
+                Console.WriteLine("nao eh possivel");
+            }
+            else
+            {
+                volumeFinal = volumeInicial - (vazao * tempo);
+                Console.WriteLine(volumeFinal);
+            }
+            Console.ReadLine();
+        }
+    }
+}
